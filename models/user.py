4ef1b5209b15eef5f57adcd27a1a052e7191633b
SavageLM <nagol.egavas@gmail.com>
2023-10-06 12:31:27 -0700

    Trying this again

--- models/user.py
+++ models/user.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Defines a Class User, that Inherits from Base_model"""
+from base_model.py import Base_model
+
+
+class User(Base_model):
+    """Defines a class User"""
+    self.email = ""
+    self.password = ""
+    self.first_name = ""
+    self.last_name = ""
