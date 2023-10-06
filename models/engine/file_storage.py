380467b856cec5564f7907201194e615fa446e02
SavageLM <nagol.egavas@gmail.com>
2023-10-06 12:14:52 -0700

    Added User class, updated file_storage to de/serialize User.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,12 +2,12 @@
 """FileStorage module"""
 import json
 from models.base_model import BaseModel
+from models.user import User
 """from models.amenity import Amenity
 from models.city import City
 from models.state import state
 from models.place import Place
-from models.review import Review
-from models.user import User"""
+from models.review import Review"""
 
 class FileStorage:
     """class FileStorage """
