73d2d42c3eb4eb92ee0e677175c653fd1a49b352
matternstsr <matternstsr@gmail.com>
2023-10-09 13:59:20 -0500

    tyring unitests for base file storage in engine and alot of flake8 issues through out several files.

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -1,74 +1,56 @@
 #!/usr/bin/python3
 """FileStorage unittests"""
-
-import os
 import unittest
+import time
+from os import remove
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+"""took import datetimedd out????"""
 
 
 class TestFileStorage(unittest.TestCase):
-    """class TestFileStorage"""
+    """class TestFileStorage """
 
-    def test_file_storage_all_method_returns_empty_dict_initially(self):
-        """FileStorage all method should return an empty dict initially"""
-        storage = FileStorage()
-        storage_dict = storage.all()
+    def test_file_storage_all_method(self):
+        """FileStorage all method contains dict of BaseModel objs"""
+        storage_instance = FileStorage()
+        storage_dict = storage_instance.all()
         self.assertIsInstance(storage_dict, dict)
-        """ Check that all values in the dictionary are instances of BaseModel"""
-        self.assertTrue(all(isinstance(obj, BaseModel)
-                            for obj in storage_dict.values()))
+        for obj_instance in storage_dict.values():
+            self.assertIsInstance(obj_instance, BaseModel)
 
-    def test_file_storage_new_method_adds_object_to_storage(self):
-        """FileStorage new method should add an object to storage"""
-        storage = FileStorage()
-        base = BaseModel()
-        storage_dict = storage.all()
-        key = '{}.{}'.format(type(base).__name__, base.id)
-        self.assertIn(key, storage_dict)
+    def test_file_storage_new_method(self):
+        """FileStorage new method adds object"""
+        base_instance = BaseModel()
+        storage_instance = FileStorage()
+        storage_dict = storage_instance.all()
+        key = '{}.{}'.format(type(base_instance).__name__, base_instance.id)
+        self.assertTrue(key in storage_dict.keys())
 
-    def test_file_storage_save_method_updates_object_timestamp(self):
-        """FileStorage save method should update the object's timestamp"""
-        storage = FileStorage()
-        base = BaseModel()
-        key = '{}.{}'.format(type(base).__name__, base.id)
-        initial_timestamp = base.updated_at
-        base.save()
-        updated_timestamp = storage.all()[key].updated_at
-        self.assertNotEqual(updated_timestamp, initial_timestamp)
+    def test_file_storage_save_method(self):
+        """FileStorage save method updates __objects"""
+        base_instance = BaseModel()
+        key = '{}.{}'.format(type(base_instance).__name__, base_instance.id)
+        base_updated_0 = base_instance.updated_at
+        storage_instance = FileStorage()
+        objs_0 = storage_instance.all()
+        dt_0 = objs_0[key].updated_at
 
-    def test_file_storage_save_method_creates_json_file(self):
-        """FileStorage save method should create a JSON file"""
-        storage = FileStorage()
-        base = BaseModel()
-        key = '{}.{}'.format(type(base).__name__, base.id)
-        base.save()
-        self.assertTrue(storage._FileStorage__file_path)
-        """Check if file path is not empty"""
+        time.sleep(0.0001)
+        base_instance.save()
+
+        base_updated_1 = base_instance.updated_at
+        objs_1 = storage_instance.all()
+        dt_1 = objs_1[key].updated_at
+
+        self.assertNotEqual(base_updated_1, base_updated_0)
+        self.assertNotEqual(dt_1, dt_0)
+
         try:
-            with open(storage._FileStorage__file_path, 'r'):
-                pass
+            with open('file.json', 'r'):
+                remove('file.json')
         except FileNotFoundError:
-            self.fail("File not found")
-
-    def test_file_storage_save_method_raises_exception_on_inv_file_path(self):
-        """FileStorage save method should raise an exception on an invalid file path"""
-        storage = FileStorage()
-        base = BaseModel()
-        base.save()
-        invalid_path = 'invalid_path.json'
-        try:
-            with open(invalid_path, 'r'):
-                pass
-        except FileNotFoundError:
-            """ Expected behavior: File is not found, which is correct"""
-            pass
-        else:
-            self.fail(f"File '{invalid_path}' should not exist")
-
-        """ Clean up by removing the invalid file path if it exists"""
-        if os.path.exists(invalid_path):
-            os.remove(invalid_path)
+            self.assertEqual(1, 2)
 
 
 if __name__ == '__main__':
