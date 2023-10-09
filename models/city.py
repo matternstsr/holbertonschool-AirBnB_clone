a190a1c7071babf5599a20af2af7fba3433d442a
SavageLM <nagol.egavas@gmail.com>
2023-10-09 07:14:49 -0700

    Added new classes and their required variables

--- models/city.py
+++ models/city.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Defines a new class, City, that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Defines a new class, City"""
+
+    state_id = ""
+    name = ""
