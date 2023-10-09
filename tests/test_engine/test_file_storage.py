f658c0ceed3237f570212e97424aa3a7837f3087
matternstsr <matternstsr@gmail.com>
2023-10-09 14:37:07 -0500

    testing def test_documentation(self):???

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -2,14 +2,38 @@
 """FileStorage unittests"""
 import unittest
 import time
+import os
 from os import remove
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-"""took import datetimedd out????"""
 
 
 class TestFileStorage(unittest.TestCase):
-    """class TestFileStorage """
+    """class TestFileStorage"""
+
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.user = BaseModel()
+        cls.user.name = "Kev"
+        cls.user.my_number = 42
+        cls.storage = FileStorage()
+        cls.path = "file.json"
+
+    @classmethod
+    def tearDownClass(cls):
+        """at the end of the test this will tear it down"""
+        del cls.user
+        """ if delete the file """
+        if os.path.exists("file.json"):
+            os.remove("file.json")
+
+    def tearDown(self):
+        """teardown"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
 
     def test_file_storage_all_method(self):
         """FileStorage all method contains dict of BaseModel objs"""
@@ -51,6 +75,21 @@ class TestFileStorage(unittest.TestCase):
                 remove('file.json')
         except FileNotFoundError:
             self.assertEqual(1, 2)
+
+    def test_docstring(self):
+        """Test docstring"""
+        self.assertIsNotNone(FileStorage.__doc__)
+
+    def test_documentation(self):
+        """Test documentation, created and not empty"""
+        self.assertTrue(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertTrue(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertTrue(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertTrue(FileStorage.reload.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
 
 
 if __name__ == '__main__':
