abd5c644f4bd777e1a1adbadf2a0c58a41f1b51e
matternstsr <matternstsr@gmail.com>
2023-10-09 23:30:26 -0500

    Cleaned up and took out some un needed junk.... We need to figure out the imports. Im not sure on that...

--- console.py
+++ console.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """Defines a Class HBNBCommand"""
 import cmd
-
+"""import models
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review"""
 
 class HBNBCommand(cmd.Cmd):
     """ Defines the class HBNBCommand"""
     intro = 'Welcome to HOlberton BNB, type help or ? to list commands.\n'
-    prompt = '(hbnb)'
+    prompt = '(hbnb) '
 
     def do_quit(self, arg):
         """Exits the Console by typing 'quit'"""
