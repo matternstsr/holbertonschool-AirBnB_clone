012b78901c15f6617d0a049a9539f4e4617b16ba
matternstsr <matternstsr@gmail.com>
2023-10-10 19:56:56 -0500

    trying more tests for unitestsing

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""FileStorage unittests"""
-import unittest
-import models
-import time
+"""Unit tests for FileStorage and its methods"""
+
 import os
-from os import remove
-from models.engine.file_storage import FileStorage
+import unittest
+import uuid
+from datetime import datetime
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
-    """class TestFileStorage"""
+    """Class to test FileStorage"""
 
     @classmethod
     def setUpClass(cls):
-        """set up for test"""
+        """Set up for the tests"""
         cls.user = BaseModel()
         cls.user.name = "Kev"
         cls.user.my_number = 42
@@ -23,15 +23,14 @@ class TestFileStorage(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        """at the end of the test this will tear it down"""
+        """Tear down at the end of the tests"""
         del cls.user
-        """ if delete the file """
         if os.path.exists("file.json"):
             os.remove("file.json")
 
     @classmethod
-    def tearDown(self):
-        """teardown"""
+    def tearDown(cls):
+        """Tear down"""
         try:
             os.remove("file.json")
         except Exception:
@@ -49,17 +48,6 @@ class TestFileStorage(unittest.TestCase):
         """Test docstring"""
         self.assertIsNotNone(FileStorage.__doc__)
 
-    def test_documentation(self):
-        """Test documentation, created and not empty"""
-        self.assertTrue(FileStorage.all.__doc__)
-        self.assertIsNotNone(FileStorage.all.__doc__)
-        self.assertTrue(FileStorage.new.__doc__)
-        self.assertIsNotNone(FileStorage.new.__doc__)
-        self.assertTrue(FileStorage.save.__doc__)
-        self.assertIsNotNone(FileStorage.save.__doc__)
-        self.assertTrue(FileStorage.reload.__doc__)
-        self.assertIsNotNone(FileStorage.reload.__doc__)
-
     def test_file_storage_type(self):
         """Test that the storage instance is of the correct type"""
         storage_instance = FileStorage()
@@ -68,7 +56,6 @@ class TestFileStorage(unittest.TestCase):
     def test_save_method_type_error(self):
         """Test that save method raises TypeError with incorrect arguments"""
         storage_instance = FileStorage()
-        """Attempt to call save with an argument is not a BaseModel instance"""
         with self.assertRaises(TypeError):
             storage_instance.save("not_a_base_model_instance")
 
@@ -79,7 +66,7 @@ class TestFileStorage(unittest.TestCase):
 
     def test_new_with_None(self):
         """Test if new method raises TypeError with None argument"""
-        with self.assertRaises(TypeError):
+        with self.assertRaises(AttributeError):
             self.storage.new(None, None)
 
     def test_save_with_None(self):
@@ -89,12 +76,12 @@ class TestFileStorage(unittest.TestCase):
 
     def test_file_path_type(self):
         """Test if the file path attribute is a string"""
-        self.assertIsInstance(self.storage._FileStorage__file_path, str)
+        self.assertIsInstance(self.storage.__file_path, str)
 
     def test_objects_type(self):
         """Test if the objects attribute is a dictionary"""
         self.assertIsInstance(self.storage.all(), dict)
-    
+
     def test_file_path_private(self):
         """Test if the file path attribute is private (starts with an underscore)"""
         self.assertTrue(hasattr(self.storage, '_FileStorage__file_path'))
@@ -103,13 +90,36 @@ class TestFileStorage(unittest.TestCase):
         """Test if the objects attribute is private (starts with an underscore)"""
         self.assertTrue(hasattr(self.storage, '_FileStorage__objects'))
 
-    def test_file_path_type(self):
-        """Test if the __file_path attribute is of type str"""
-        self.assertEqual(type(models.storage._FileStorage__file_path), str)
+    def test_base_model_id_is_string(self):
+        """UUID format testing."""
+        bm = BaseModel()
+        self.assertIsInstance(bm.id, str)
 
-    def test_objects_types(self):
-        """Test if the objects attribute is of type dict"""
-        self.assertEqual(type(models.storage._FileStorage__objects), dict)
-        
+    def test_base_model_uuid_good_format(self):
+        """Tests if UUID is in the correct format."""
+        bm = BaseModel()
+        self.assertIsInstance(uuid.UUID(bm.id), uuid.UUID)
+
+    def test_base_model_created_at_is_datetime(self):
+        """Datetime test."""
+        bm = BaseModel()
+        self.assertIsInstance(bm.created_at, datetime)
+
+    def test_base_model_updated_at_is_datetime(self):
+        """Datetime test."""
+        bm = BaseModel()
+        self.assertIsInstance(bm.updated_at, datetime)
+
+    def test_creation_from_dictionary_basic(self):
+        """This function proves in a basic way that when instantiating by passing a dictionary, it works correctly."""
+        date = datetime.now()
+        dic = {"id": "7734cf23-6c89-4662-8483-284727324c77", "created_at": "2020-02-17T16:32:39.023915", "updated_at": "2020-02-17T16:32:39.023940", "__class__": "BaseModel"}
+        my_base = BaseModel(**dic)
+        self.assertEqual(my_base.__class__.__name, "BaseModel")
+        self.assertEqual(my_base.id, "7734cf23-6c89-4662-8483-284727324c77")
+        self.assertEqual(type(my_base.created_at), type(date))
+        self.assertEqual(type(my_base.updated_at), type(date)
+
+
 if __name__ == '__main__':
     unittest.main()
