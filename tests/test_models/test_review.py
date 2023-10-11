18914a67885c8b7736a12d1c609c968057fcd143
matternstsr <matternstsr@gmail.com>
2023-10-10 22:33:29 -0500

    trying a few more unitests of the attributes.

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -2,13 +2,9 @@
 import unittest
 from models.base_model import BaseModel
 from models.review import Review
-"""from models.state import State
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place"""
 
 
-class Testreview(unittest.TestCase):
+class TestReview(unittest.TestCase):
     """Unittests for the Review Class"""
 
     def test_class(self):
@@ -41,7 +37,7 @@ class Testreview(unittest.TestCase):
             self.assertTrue(len(func.__doc__) > 0)
 
     def test_is_instance(self):
-        """ Test if user is instance of basemodel """
+        """ Test if user is an instance of BaseModel """
         my_Review = Review()
         self.assertTrue(isinstance(my_Review, BaseModel))
 
@@ -51,3 +47,45 @@ class Testreview(unittest.TestCase):
         self.assertTrue(type(my_Review.place_id) is str)
         self.assertTrue(type(my_Review.user_id) is str)
         self.assertTrue(type(my_Review.text) is str)
+
+    def test_review(self):
+        ''' test 'Review' exists '''
+        inst_1 = Review()
+        self.assertTrue(inst_1)
+
+    def test_review_instance_del(self):
+        ''' test 'Review' deletes '''
+        inst_1_1 = Review()
+        del inst_1_1
+
+    def test_review_instance(self):
+        ''' test 'Review' instance '''
+        inst_2 = Review()
+        self.assertIsInstance(inst_2, Review)
+
+    def test_review_save(self):
+        ''' test 'Review' saves '''
+        inst_3 = Review()
+        updated_review = inst_3.updated_at
+        inst_3.save()
+        new_inst_3 = inst_3.updated_at
+        self.assertNotEqual(updated_review, new_inst_3)
+
+    def test_place_id_str(self):
+        ''' test 'Review' type '''
+        inst_4 = Review()
+        self.assertIsInstance(inst_4.place_id, str)
+
+    def test_user_id_str(self):
+        ''' test 'Review' type '''
+        inst_5 = Review()
+        self.assertIsInstance(inst_5.user_id, str)
+
+    def test_text_str(self):
+        ''' test 'Review' type '''
+        inst_6 = Review()
+        self.assertIsInstance(inst_6.text, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -3,51 +3,48 @@
 import unittest
 from models.state import State
 from models.base_model import BaseModel
-"""from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.user import User
-from models.review import Review
-from models.amenity import Amenity
-from models.base_model import BaseModel"""
 
 
 class Teststate(unittest.TestCase):
     """Unittests for the State Class"""
 
     def test_class(self):
-        """Tests for correct class"""
+        """Tests for the correct class"""
         state1 = State()
         self.assertEqual(state1.__class__.__name__, "State")
 
     def test_inheritance(self):
-        """Tests if class inherits from BaseModel"""
+        """Tests if the class inherits from BaseModel"""
         state1 = State()
         self.assertEqual(state1.__class__.__name__, "State")
 
     def setUp(self):
-        """Return to "" class attributes"""
+        """Return the 'name' class attribute to its default value"""
         State.name = ""
 
     def test_module_doc(self):
-        """ check for module documentation """
+        """Check for module documentation"""
         self.assertTrue(len(State.__doc__) > 0)
 
     def test_class_doc(self):
-        """ check for documentation """
+        """Check for class documentation"""
         self.assertTrue(len(State.__doc__) > 0)
 
     def test_method_docs(self):
-        """ check for method documentation """
+        """Check for method documentation"""
         for func in dir(State):
             self.assertTrue(len(func.__doc__) > 0)
 
     def test_is_instance(self):
-        """ Test if user is instance of basemodel """
+        """Test if a state is an instance of BaseModel"""
         my_state = State()
         self.assertTrue(isinstance(my_state, BaseModel))
 
     def test_field_types(self):
-        """ Test field attributes of user """
+        """Test field attributes of the state"""
         my_state = State()
         self.assertTrue(type(my_state.name) is str)
+
+
+if __name__ == '__main__':
+    unittest.main()
